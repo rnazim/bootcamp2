b056a17a3dc4f8186a2cbf8680a2be90eb8de9b3
Rahmad Nur Azim <rahmad.azim@aux.dkatalis.com>
2022-11-08 13:59:04 +0700

    add file

---
+++ src/main/java/com/bcafinance/training/day2.java
@@ -0,0 +1,24 @@
+package com.bcafinance.training;
+
+/*
+@Author Azim a.k.a. Rahmad Nur Azim
+Principle Architect
+Created with Eclipse version 4.25.0
+Created on 08-11-2022 10:23:29
+Last Modified on 08-11-2022 10:23:29
+Version 1.0
+*/
+
+public class day2 {
+	public static void main(String[] args) {
+		
+		char test = '9';
+		char test2 = '8';
+		System.out.println(test);
+		System.out.println(test2);
+		
+		//System.out.println(test - test2);
+	}
+	
+
+}
