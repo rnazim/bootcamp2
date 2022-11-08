2417077c2127f62d329706e7061b0d5bc4ed5e41
Rahmad Nur Azim <rahmad.azim@aux.dkatalis.com>
2022-11-08 14:18:26 +0700

    add value day2.java

--- src/main/java/com/bcafinance/training/day2.java
+++ src/main/java/com/bcafinance/training/day2.java
@@ -13,7 +13,7 @@ public class day2 {
 	public static void main(String[] args) {
 		
 		char test = '9';
-		char test2 = '8';
+		char test2 = '1';
 		System.out.println(test);
 		System.out.println(test2);
 		
